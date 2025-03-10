0e47a5cd91532cd6fe0e55b2b8c0a69a097c9d40
Tansil011019 <wilsontansil@gmail.com>
2025-03-10 17:48:15 +0700

    chore: setting up

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Stop script on error
+set -e
+
+echo "Starting Setting Up"
+
+VENV_DIR="venvs/efficient-rsnn-bmi"
+
+# Check if virtual environment exists
+if [ ! -d "$VENV_DIR"]; then
+    echo "No virtual environment found, creating one..."
+    python3 -m venv $VENV_DIR
+fi
+
+# Activate the virtual environment
+source "$VENV_DIR/bin/activate"
+
+pip install --upgrade pip
+python3 -m pip install --require-virtualenv -e "."
+
+echo "Project is completely set"
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from omegaconf import DictConfig
+
+def train_rsnn_tiny(cfg: DictConfig):
+    pass
+
+if __name__ == "__main__":
+    train_rsnn_tiny()
